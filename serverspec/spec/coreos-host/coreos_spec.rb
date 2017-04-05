9e8d5cc0b7e0d71e2da83358bffd859ee2ebcf16
Dennis Arslan <dennis.arslan@ictu.nl>
2017-04-05 17:44:33 +0200

    Adding tests

--- serverspec/spec/coreos-host/coreos_spec.rb
+++ serverspec/spec/coreos-host/coreos_spec.rb
@@ -3,17 +3,61 @@ require 'spec_helper'
 =begin
 Ensure authorized keys of all IQT members are set and other keys are NOT present
 Ensure timesyncd.conf is set with our NTP servers
-Ensure root ssh login is not allowed in /etc/ssh/sshd_config
-Ensure inotify-max-user-instances is set to 8192
-Ensure fs.inotify.max_user_instances is set to 8192
 Ensure host is registered to Nodemanager
-Ensure eth0 is up and configured
-Ensure eth1 (vlan tagging) is up
-Ensure eth2 (nfs) is up and configured
-Ensure host has mount to nfs server
-Ensure /shared/data directory exists
-Ensure /local/data directory exists
 =end
+
+describe 'Linux kernel parameters' do
+  context linux_kernel_parameter('fs.inotify.max_user_instances') do
+    its(:value) { should eq 8192 }
+  end
+end
+
+=begin
+describe user('core') do
+  it { should have_authorized_key '' }
+end
+=end
+
+=begin
+Next one fails, might be permissions issue, running as user core, does not have access to this file.
+=end
+
+describe file('/etc/ssh/sshd_config') do
+   it { should contain 'PermitRootLogin no' }
+end
+
+describe file('/local/data') do
+  it { should be_mounted.with( :type => 'ext4' ) }
+end
+
+describe file('/mnt/data') do
+  it { should be_mounted.with( :type => 'nfs4' ) }
+end
+
+describe file('/shared/data') do
+  it { should be_mounted.with( :type => 'nfs4' ) }
+end
+
+describe file('/var/lib/docker') do
+  it { should be_mounted.with( :type => 'btrfs' ) }
+end
+
+describe interface('eth0') do
+  it { should exist }
+  it { should be_up }
+  its(:ipv4_address) { should contain '10.' }
+end
+
+describe interface('eth1') do
+  it { should exist }
+  it { should be_up }
+end
+
+describe interface('eth2') do
+  it { should exist }
+  it { should be_up }
+  its(:ipv4_address) { should contain '192.168.' }
+end
 
 describe service('docker') do
   it { should be_enabled }
@@ -40,12 +84,12 @@ describe docker_container('vmware-tools') do
   it { should be_running }
 end
 
-describe docker_container('infra-dashboard-agent') do
+describe docker_container('dashboard-agent-infra') do
   it { should exist }
   it { should be_running }
 end
 
-describe docker_container('infra-dashboard-publisher') do
+describe docker_container('bigboat-publisher-infra') do
   it { should exist }
   it { should be_running }
 end
